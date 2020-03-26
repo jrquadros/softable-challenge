de9e5a979856811366198de1789fa8d5fc82bafc
Júnior Quadros <jrmquadros@gmail.com>
2020-03-26 15:08:55 -0300

    chore(app): update eslint config

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { StartupCard } from './components/StartupCard'
+import { GlobalStyle } from './components/GlobalStyle'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+
+  padding: 20px;
+
+  width: 100vw;
+  height: 100vh;
+
+  background-color: #f0f0ee;
+`
 
 function App() {
   return (
     <div className="App">
-      <h1>Challenge</h1>
+      <GlobalStyle />
+      <Container>
+        <StartupCard
+          id={'asdf'}
+          imageUrl={'https://www.eaalim.com/download/wp-content/uploads/2014/01/hellfire.jpg'}
+          segment={'NomeDaStartUp'}
+          name={'Biotech'}
+        />
+      </Container>
     </div>
   )
 }
